459659c76effaa507c52a1ac69ef874cbca4cbb4
Benson Wong <mostlygeek+git@gmail.com>
2015-06-29 17:45:02 -0700

    add configuration via env variables

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
+	"os"
 	"regexp"
 	"strconv"
 	"strings"
@@ -14,26 +16,58 @@ import (
 	"github.com/DataDog/datadog-go/statsd"
 )
 
-var metricPattern *regexp.Regexp
+var (
+	metricPattern *regexp.Regexp
+
+	// configurable values
+	tags      = []string{}
+	namespace string
+	listen    string
+)
 
 func init() {
 	metricPattern = regexp.MustCompile(`^[a-z][\.a-z0-9]*[a-z]$`)
+
+	tagPattern := regexp.MustCompile(`^[a-zA-Z][a-zA-Z0-9]*(:[a-zA-Z_0-9]*)?$`)
+	for _, v := range strings.Split(os.Getenv("TAGS"), ",") {
+		tag := strings.TrimSpace(v)
+		if tag != "" && tagPattern.Match([]byte(tag)) == false {
+			log.Fatalf("Invalid Tag: %s, must match: %s\n", tag, tagPattern.String())
+		}
+		tags = append(tags, tag)
+	}
+
+	namespacePattern := regexp.MustCompile(`^[a-z][a-z0-9\.]*[a-z0-9]\.$`)
+	namespace = strings.TrimSpace(os.Getenv("NAMESPACE"))
+	if namespace == "" {
+		namespace = "experimental."
+	}
+
+	if namespacePattern.Match([]byte(namespace)) == false {
+		log.Fatalf("Invalid namespace `%s`, must match: %s\n", namespace, namespacePattern.String())
+	}
+
+	listen = os.Getenv("LISTEN")
+	if listen == "" {
+		listen = ":8080"
+	}
+}
+
+func metricNameOK(metricName string) bool {
+	return metricPattern.Match([]byte(metricName))
 }
 
 func extractMetric(requestUrl *url.URL) (string, error) {
-
 	parts := strings.Split(requestUrl.Path, "/")
 	if len(parts) < 2 {
 		return "", fmt.Errorf("No metric provided")
 	}
 
-	metricName := parts[2]
-	// check that metric name matches a pattern
-	if metricPattern.Match([]byte(metricName)) == false {
-		return "", fmt.Errorf("does not match pattern")
+	if !metricNameOK(parts[2]) {
+		return "", fmt.Errorf("Invalid metric name")
 	}
 
-	return metricName, nil
+	return parts[2], nil
 }
 
 func getString(r *http.Request) (string, error) {
@@ -84,6 +118,8 @@ func makeHandler(handler statHandler) http.HandlerFunc {
 		if err := handler(metricName, r); err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			bytes.NewBufferString("ERROR: " + err.Error() + "\n").WriteTo(w)
+		} else {
+			io.WriteString(w, "OK")
 		}
 	}
 }
@@ -95,10 +131,6 @@ func main() {
 		log.Fatal(err)
 	}
 	ddClient.Namespace = "experimental."
-
-	tags := []string{
-		"env:dev",
-	}
 
 	http.HandleFunc("/gauge/", makeHandler(func(m string, r *http.Request) error {
 		val, err := getFloat64(r)
@@ -132,5 +164,10 @@ func main() {
 		return ddClient.Set(m, val, tags, 1)
 	}))
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Printf("Namespace	: %s\n", namespace)
+	fmt.Printf("Tags		: %s\n", tags)
+	fmt.Printf("Listen		: %v\n", listen)
+
+	log.Fatal(http.ListenAndServe(listen, nil))
+
 }
